fd50f458ae25df2f7ac68cdfe2eb1fb2879ec871
matijaskorup1991 <smatija1991@gmail.com>
2021-06-24 16:40:31 +0200

    bug

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import './App.css';
-<<<<<<< HEAD
+
 import TeamDetailView from './pages/TeamDetailView';
+import Header from './components/Header/Header';
+import LeagueDetailView from './pages/LeagueDetailView';
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <TeamDetailView />
+      <LeagueDetailView></LeagueDetailView>
     </div>
   );
-=======
-import Header from './components/Header/Header';
-
-import LeagueDetailView from './pages/LeagueDetailView'
-
-function App() {
-  return <div className='App'>
-<<<<<<< HEAD
-    <Header/>
-=======
-    <LeagueDetailView></LeagueDetailView>
->>>>>>> 39b9868118a82004899e59aa62cec3e51c43484f
-  </div>;
->>>>>>> be90cbfcc3d1465dc63786d289ae21436afbf096
 }
 
 export default App;
