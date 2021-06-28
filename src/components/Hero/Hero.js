675c5b9107ae3a50c8a5daa563e23487726ccd91
wlsp <wlsp@outlook.de>
2021-06-28 13:10:04 +0200

    70%

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,14 +6,14 @@ import Arrowup from '../../images/arrow-up.png';
 const Hero = () => {
     return (<header>
         <figure>
-            <img src={HeaderImg} alt='' />
+            <img src={HeaderImg} alt='baseball base' />
             <figcaption>
                 <h1>Find Your League</h1>
             </figcaption>
         </figure>
         <aside>
             <span className="arrow-up">
-                <img src={Arrowup} />
+                <img src={Arrowup} alt='Up-Arrow' />
             </span>
         </aside>
     </header>
