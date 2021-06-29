7f0da343d72b709cb33c7c68259227a8eea3c339
matijaskorup1991 <smatija1991@gmail.com>
2021-06-29 14:24:32 +0200

    searchBox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
+import React, { useState } from 'react';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from 'react-router-dom';
 
 import TeamDetailView from './pages/TeamDetailView';
 import Navbar from './components/Navigation/Navbar';
@@ -8,18 +14,30 @@ import LeagueDetailView from './pages/LeagueDetailView';
 import HomePage from './pages/HomePage';
 import NotFound from './pages/NotFound';
 
-function App() {
+function App({ history }) {
+  let [searchLetter, setSearchLetter] = useState('');
+
+  console.log('search term is: ', searchLetter);
+
+  function handleSearchTerm(e) {
+    console.log(e);
+    setSearchLetter(searchLetter + e.key);
+    if (e.key === 'Enter') {
+      history.push(`/${searchLetter}`);
+    }
+  }
+
   return (
     <Router>
-      <Navbar />
+      <Navbar searchTerm={handleSearchTerm} appState={searchLetter} />
       <Switch>
         <Route path='/team/:id' exact component={TeamDetailView} />
         <Route path='/league/:id' exact component={LeagueDetailView} />
-        <Route path='/' exact component={HomePage} />
+        <Route path='/:id?' exact component={HomePage} />
         <Route exact path='*' component={NotFound} />
       </Switch>
     </Router>
   );
 }
 
-export default App;
+export default withRouter(App);
--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,28 +1,35 @@
 import React from 'react';
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
 
 import './Navbar.scss';
 import ArrowLogo from '../../images/arrow-logo.svg';
 import SearchIcon from '../../images/search-icon.svg';
 
-
-const Header = () => {
-    return (
-            <nav>
-                <Link exact to="/">
-                <span className='logo-box'>
-                    <img src={ArrowLogo} alt='' />
-                    <h4>Sports.db</h4>
-                </span>
-                </Link>
-                <form className='search-box'>
-                    <span className='search-btn'>
-                        <img src={SearchIcon} alt='' />
-                    </span>
-                    <input className='search' type='text'></input>
-                </form>
-            </nav>
-    );
+const Header = ({ searchTerm, appState }) => {
+  console.log('search Term:', searchTerm);
+  return (
+    <nav>
+      <Link exact to='/'>
+        <span className='logo-box'>
+          <img src={ArrowLogo} alt='' />
+          <h4>Sports.db</h4>
+        </span>
+      </Link>
+      <form className='search-box'>
+        <span className='search-btn'>
+          <img src={SearchIcon} alt='' />
+        </span>
+        <input
+          className='search'
+          type='text'
+          value={appState}
+          onKeyDown={(e) => {
+            searchTerm(e);
+          }}
+        ></input>
+      </form>
+    </nav>
+  );
 };
 
 export default Header;
